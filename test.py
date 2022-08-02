b2598814b6d4b4edadadebf7bf0fbb6121594794
蔡淵丞 <86534155+Vincent1837@users.noreply.github.com>
2022-08-02 22:50:17 +0800

    .

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ import pandas_datareader.data as web
 from talib import abstract
 from datetime import datetime, timedelta
 
-start = datetime.now()- timedelta(days =  10)
+start = datetime.now()- timedelta(days =  100)
 end =  datetime.now()
-stock = web.DataReader('2330.TW', 'yahoo', start, end)
+stock = web.DataReader('8069.TWO', 'yahoo', start, end)
 print(stock)
-stock = mpf.plot(stock, type = "candle")
-stock.show()
+stock = mpf.plot(stock, type = "candle", mav = (5, 10, 20))
+plt.show()
