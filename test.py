168d54d702ad665c821e4828a8ae5253c7e3fd97
shawn <72637628+Shawn-lung@users.noreply.github.com>
2022-08-08 20:05:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import requests as request
+import json
 url = request.get("https://tw.screener.finance.yahoo.net/future/q?type=ta&perd=d&mkt=01&sym=WTX00&callback=jQuery1113033265881543586406_1659958858733&_=1659958858734v")
-url
+a = url.text.replace('jQuery1113033265881543586406_1659958858733(','')
+a = a.replace(");",'')
